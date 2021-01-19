d34fa7fa6865cab3322fdc1d5091a9659ea43d09
Jacob Lindgren <jlindgre@redhat.com>
2021-01-19 11:06:06 -0600

    update commit_and_pr_all_repos.sh with more parameters

--- commit_and_pr_all_repos.sh
+++ commit_and_pr_all_repos.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-REPOS="approval/api/ catalog/api/ catalog/minion/ topo/sources-api/ topo/sources-monitor/ topo/ansible_tower/ topo/api/ topo/azure/ topo/amazon/ topo/openshift/ topo/persister/ topo/satellite/ topo/orchestrator/ topo/ingress/ topo/topological_inventory-sync"
+REPOS="approval/api/ catalog/api/ sources/api/ sources/monitor topo/ansible_tower/ topo/api/ topo/azure/ topo/amazon/ topo/openshift/ topo/persister/ topo/satellite/ topo/orchestrator/ topo/ingress/ topo/sync topo/scheduler topo/google"
+
+PREV="2.5"
+NEW="2.6"
+BRANCH_NAME=update_ubi_image
 
 for i in $REPOS ; do
     (
@@ -8,8 +13,9 @@ for i in $REPOS ; do
         cd $i && \
             git co master && \
             git pull upstream master && \
-            git co -b update_ubi_image && \
-            git apply CHANGEME && \
+            git push origin master && \
+            git co -b ${BRANCH_NAME} && \
+            sed -i "s/${PREV}/${NEW}/g" Dockerfile
             git add Dockerfile && \
             git commit -m "$1" && \
             gh pr create -t "$1" -b "$2"
